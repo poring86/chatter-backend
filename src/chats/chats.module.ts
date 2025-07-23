0e034c15e637e0c4c63e9bd3f93dc917acd15ccc
poring86 <matheuslino86a@gmail.com>
2025-07-23 17:04:40 -0400

    Messages

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,10 +4,12 @@ import { ChatsResolver } from './chats.resolver';
 import { DatabaseModule } from '../common/database/database.module';
 import { Chat, ChatSchema } from './entities/chat.entity';
 import { ChatsRepository } from './chats.repository';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
     DatabaseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    MessagesModule,
   ],
   providers: [ChatsResolver, ChatsService, ChatsRepository],
 })
