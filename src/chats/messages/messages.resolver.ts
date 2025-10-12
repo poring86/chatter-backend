5f2ede4a180967aca49ae2b516817a07e0642423
poring86 <matheuslino86a@gmail.com>
2025-10-12 17:03:42 -0400

    create message

--- src/chats/messages/messages.resolver.ts
+++ src/chats/messages/messages.resolver.ts
@@ -1,7 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { MessagesService } from './messages.service';
+import { Message } from './entities/message.entity';
+import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
+import { CreateMessageInput } from './dto/create-message.input';
+import { TokenPayload } from 'src/auth/token-payload.interface';
+import { UseGuards } from '@nestjs/common';
+import { CurrentUser } from 'src/auth/current-user.decorator';
 
-@Resolver()
+@Resolver(() => Message)
 export class MessagesResolver {
   constructor(private readonly messagesService: MessagesService) {}
+
+  @Mutation(() => Message)
+  @UseGuards(GqlAuthGuard)
+  async createMessage(
+    @Args('createMessageInput') createMessageInput: CreateMessageInput,
+    @CurrentUser() user: TokenPayload,
+  ) {
+    return this.messagesService.createMessage(createMessageInput, user._id);
+  }
 }
